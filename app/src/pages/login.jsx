cf4a642cf65730e78f478d509bec8cdd4f00cf36
DANA ISABELLA DUARTE LINARES <daduarteli@est.utn.ac.cr>
2026-07-29 03:57:44 -0600

    inicio del login

---
+++ app/src/pages/login.jsx
@@ -0,0 +1,47 @@
+/*import {useState} from 'react';
+import {Eye, EyeOff, Mail,Lock} from "lucide-react";
+import {Button} from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {label } from "@/components/ui/label";   
+
+
+export default function login(){
+    //mostrar y ocultar la contraseña al hacer clic en el ojito 
+    const [mostrarPassword, setMostrarPassword] = useState(false);
+
+    return(
+        <div className= "min-h-screen grid grid-cols-1 md:grid-cols-2 bg-[#F5F1EA]"> 
+        { //lado izquierdo
+            <div className="hidden md:flex flex-col items-center justify-center p-12 border-r border-blanck/10">
+        <img> 
+        src="/src/assets/casa.png"
+        alt= "Okinawa Tattoo Studio"
+        className="max-w-sm" 
+        </img>
+
+        <p className= "font-serif text-2xl mt-8">Arte que se queda contigo.</p>
+        <p className= "text-center text-neutral-600 mt-2" > Tatuajes con historia.<br />Hechos con intención en Okinawa. </p>
+
+        </div>
+            }
+        
+
+            //lado derecho
+            <div className="flex flex-col justify-center px-8 md:px-20 py-12">
+                //header
+                <div className="flex justify-between items-center mb-16">
+                    <div>
+                        <h1 className="font-serif text-2xl font-bold">Okinawa</h1>
+                        <p className="text-xs tracking-normal text-neutral-500">TATTOO STUDIO</p>
+                    </div>
+                < form className="space-y-6 max-w-md"> 
+                {
+
+                }
+                
+            <p className="text-red-600 text-sm font-semibold tracking-wide mb-2">Bienvenido de nuevo</p>
+            
+      <h2 className="font-serif text-5xl mb-2">Inicia sesión</h2>
+      <p className="text-red-600 mb-10> y reserva tu próxima obra de arte.</p>
+
+*/
